b27e465f81cfee47e553ba00d1f93a90b98332e3
91d906h4 <fairymanning@gmail.com>
2024-03-29 19:57:26 +0900

    update

--- model/data.py
+++ model/data.py
@@ -101,7 +101,9 @@ class Data:
 
             for token in jieba.cut(sentance):
                 # Remove keywords.
-                if token in set(["牡羊座", "金牛座", "雙子座", "巨蟹座", "獅子座", "處女座", "天秤座", "天蠍座", "射手座", "摩羯座", "水瓶座", "雙魚座"]): continue
+                # There are two names of Aries in Chinese, one is 白羊座,
+                # the other is 牡羊座, so we need to remove both of them.
+                if token in set(["白羊座", "牡羊座", "金牛座", "雙子座", "巨蟹座", "獅子座", "處女座", "天秤座", "天蠍座", "射手座", "摩羯座", "水瓶座", "雙魚座"]): continue
 
                 self.tokens.add(token)
                 temp.append(token)
@@ -128,6 +130,11 @@ class Data:
 
         # Create word-to-index index.
         self.w2i = {word: index for index, word in enumerate(self.tokens)}
+
+        # Output word2index dict.
+        with open("./word2index.txt", "+w") as f:
+            for _, k in enumerate(self.w2i):
+                f.write(f"\"{k}\": {self.w2i[k]},\n")
 
         for sentance, index in self.dataset:
             temp = []
